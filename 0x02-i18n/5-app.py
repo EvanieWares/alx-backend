29cc45365b1a734c4986a7cea7e660859904b2b2
EvanieWares <evaniewares@gmail.com>
2024-05-14 23:38:54 +0200

    mock logging in

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-
 """
-5. Basic Flask app
+Basic Flask app
 """
-
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
@@ -14,6 +12,7 @@ babel = Babel(app)
 class Config:
     """
     Config class.
+    Contains configuration for the Babel plugin.
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
@@ -32,7 +31,21 @@ users = {
 
 def get_user(login_as):
     """
-    get_user.
+    Get the user associated with a given login_as value.
+
+    Parameters
+    ----------
+    login_as : str
+        The value of the login_as argument to a URL. This is the
+        value of the g.user variable after calling the
+        before_request function.
+
+    Returns
+    -------
+    user : dict
+        A dictionary representing the user associated with the
+        given login_as value. If no user is associated with
+        the given login_as value, this will be None.
     """
     try:
         return users.get(int(login_as))
@@ -43,7 +56,15 @@ def get_user(login_as):
 @app.before_request
 def before_request():
     """
-    before_request
+    Set the g.user variable in the global context of the request.
+
+    This function is called before each request and sets the
+    g.user variable based on the "login_as" argument in the query
+    string. The g.user variable is used in the templates to
+    display the user's name and other information.
+
+    If the "login_as" argument is not present, the g.user variable
+    is set to None.
     """
     g.user = get_user(request.args.get("login_as"))
 
@@ -51,7 +72,16 @@ def before_request():
 @babel.localeselector
 def get_locale():
     """
-    get_locale.
+    Determine the best match of the incoming request's locale
+    from the LANGUAGES configuration variable.
+
+    This is done by looking at the best match of the
+    `accept_languages` attribute of the request object
+    against the LANGUAGES configuration variable.
+
+    Returns:
+        str: The best match of the incoming request's locale
+             from the LANGUAGES configuration variable.
     """
     locale = request.args.get("locale")
     if locale:
@@ -61,9 +91,7 @@ def get_locale():
 
 @app.route('/', methods=["GET"], strict_slashes=False)
 def hello():
-    """
-    hello.
-    """
+    """Returns the 0-index.html template."""
     return render_template('5-index.html')
 
 
